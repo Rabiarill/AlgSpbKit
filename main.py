7db2d4acac295630799b6c9954753db36bc723b7
Rabiarill <doomreanger@mail.ru>
2022-10-13 18:16:17 +0300

    update with functions

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import math
 
-select = int(input("Выберите:\n1.Сложение\n2.Умножение\n3.Деление\n4.Вычитание\n5.Квадратное уравнение\n"))
-while select > 5 or select < 1:
-    select = int(input())
 
-if select <= 4 and select >= 1:
+def requestTwoNubers():
+    global firstNumber, secondNumber
     firstNumber = int(input("Введите первое число\n"))
     secondNumber = int(input("Введите второе число\n"))
-    if select == 1:
-        print(firstNumber + secondNumber)
-    if select == 2:
-        print(firstNumber * secondNumber)
-    if select == 3:
-        print(firstNumber / secondNumber)
-    if select == 4:
-        print(firstNumber - secondNumber)
+
+
+def requestThreeNumbers():
+    global a, b, c
+    a = int(input("Введите а\n"))
+    b = int(input("Введите b\n"))
+    c = int(input("Введите c\n"))
+
+
+def solveQuadraticEquation():
     if select == 5:
-        a = int(input("Введите а\n"))
-        b = int(input("Введите b\n"))
-        c = int(input("Введите c\n"))
+        requestThreeNumbers()
         D = b ** 2 - 4 * a * c
         if D > 0:
             ans_1 = (-b + math.sqrt(D)) / (2 * a)
@@ -30,3 +28,22 @@ if select <= 4 and select >= 1:
         if D < 0:
             print("Корней нет")
 
+
+def decideOnAction():
+    if select == 1:
+        print(firstNumber + secondNumber)
+    if select == 2:
+        print(firstNumber * secondNumber)
+    if select == 3:
+        print(firstNumber / secondNumber)
+    if select == 4:
+        print(firstNumber - secondNumber)
+
+
+while True:
+    select = int(input("Выберите:\n1.Сложение\n2.Умножение\n3.Деление\n4.Вычитание\n5.Квадратное уравнение\n"))
+    if select <= 4 and select >= 1:
+        requestTwoNubers()
+        decideOnAction()
+    elif select == 5:
+        solveQuadraticEquation()
